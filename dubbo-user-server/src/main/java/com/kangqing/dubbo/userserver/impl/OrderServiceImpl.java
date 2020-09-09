76254479f2030dd3a851d249f3e29f0392a68e6b
zl_yunqing163 <yunqing_71@163.com>
2020-09-09 16:06:05 +0800

    user-server用户服务模块，暴露服务@DubboService，注册到nacos注册中心

---
+++ dubbo-user-server/src/main/java/com/kangqing/dubbo/userserver/impl/OrderServiceImpl.java
@@ -0,0 +1,18 @@
+package com.kangqing.dubbo.userserver.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.kangqing.dubbo.mapper.OrderMapper;
+import com.kangqing.dubbo.pojo.Order;
+import com.kangqing.dubbo.service.OrderService;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author yx
+ * @date 2020/9/9 15:36
+ */
+@DubboService
+@Service
+public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements OrderService {
+
+}
---
+++ dubbo-user-server/src/test/java/com/kangqing/dubbo/userserver/DubboUserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangqing.dubbo.userserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboUserServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
